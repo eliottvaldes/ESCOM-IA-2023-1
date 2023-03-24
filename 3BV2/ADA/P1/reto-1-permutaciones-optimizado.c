59a6fb654902ec350cad636c2ce886d79955422d
Eliot Valdes <eliottvaldes@hotmail.com>
2023-03-24 17:16:33 -0600

    add -> creacion de archivos que contienen retos no obligatorios de creacion de permutaciones

---
+++ 3BV2/ADA/P1/reto-1-permutaciones-optimizado.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(char *x, char *y)
+{
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permutaciones(char *str, int inicio, int fin)
+{
+    if (inicio == fin)
+    {
+        fputs(str, stdout);
+        putchar('\n');
+    }
+    else
+    {
+        for (int i = inicio; i <= fin; i++)
+        {
+            swap((str + i), (str + inicio));
+            permutaciones(str, inicio + 1, fin);
+            swap((str + i), (str + inicio)); // backtrack
+        }
+    }
+}
+
+int main()
+{
+    char input[100];
+    printf("Introduce una cadena de caracteres: ");
+    scanf("%s", input);
+
+    int n = strlen(input);
+
+    printf("Las permutaciones posibles de '%s' son:\n", input);
+    permutaciones(input, 0, n - 1);
+
+    return 0;
+}
