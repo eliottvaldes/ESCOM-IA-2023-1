7b541835a5fe4d7e255189e4ef351ca723b88b15
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-05 15:50:02 -0600

    ren: set new file name using more descriptive

--- 3BV2/ADA/P3/Ejercicios/01-isPrime-bruteForce.js
+++ 3BV2/ADA/P3/Ejercicios/01-isPrime-bruteForce.js
@@ -1,24 +1,24 @@
 /**
- * Funcion que determina si un numero es primo o no
+ * Function that determines if a number is prime or not
  * @param {*} num 
  * @param {*} seed 
- * @returns 1 si es primo, 0 si no es primo
+ * @returns 1 if it is prime, 0 if it is not prime
  */
 const isPrime = (num, seed) => {
-    // definimos caso base para el numero 1
+    // define base case for number 1
     if ((seed * seed) > num) {
         return 1;
     }
-    // usamos el modulo para saber si es divisible y retornamos 0 si es divisible
+    // use the module to know if it is divisible and return 0 if it is divisible
     if (num % seed == 0) {
         return 0;
     }
-    // llamamos de manera recursiva
+    // call recursively
     return isPrime(num, seed + 1);
 }
 
 
 // test the code
-const num = 48;
+const num = 137;
 const seed = 2;
 console.log(`El numero ${num} es primo? ${isPrime(num, seed) === 0 ? 'No' : 'Si'}`);
