b7e918c8cd1f9d57b25be41b24dea0e5d4d3010c
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-14 23:59:10 -0600

    DB: feat: script y modulo nuevo ajustado a las delegaciones ordenadas

---
+++ 3BV2/BD/Examen/01-ocupantes-vivienda-cuarto-improved.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const fastcsv = require('fast-csv');
+const ObjectsToCsv = require('objects-to-csv');
+
+const { obtenerIDDelegacion } = require('./helper-delegaciones-improved');
+
+const stream = fs.createReadStream('./initialFiles/dataset_inegi-ocupantes-vivienda-y-cuarto.csv');
+
+let ocupacionPorDelegacion = [];
+let ocupacionVivienda = [];
+let ocupacionCuarto = [];
+
+
+// verify if the folder 'cleanFilesImproved' exists if not create it
+if (!fs.existsSync('./cleanFilesImproved')) {
+    fs.mkdirSync('./cleanFilesImproved');
+}
+
+let csvStream = fastcsv.parse()
+    .on('data', (data) => {
+
+        if (data[5] == 'Total del Municipio') {
+            ocupacionPorDelegacion.push({
+                'delegacion': data[3],
+                'por_vivienda': data[6],
+                'por_cuarto': data[7]
+            });
+        }
+
+    })
+    .on('end', async () => {
+
+        ocupacionPorDelegacion.forEach(({ delegacion, por_vivienda }) => {
+            ocupacionVivienda.push({
+                id_delegacion: obtenerIDDelegacion((delegacion.replaceAll(' ', '_').trim().toUpperCase())),
+                ocupacion_por_vivienda: por_vivienda
+            });
+        });
+
+        ocupacionPorDelegacion.forEach(({ delegacion, por_cuarto }) => {
+            ocupacionCuarto.push({
+                id_delegacion: obtenerIDDelegacion((delegacion.replaceAll(' ', '_').trim().toUpperCase())),
+                ocupacion_por_cuarto: por_cuarto,
+            });
+        });
+
+        // creacion de lso archivos csv
+
+        const csvVivienda = new ObjectsToCsv(ocupacionVivienda);
+        await csvVivienda.toDisk('./cleanFilesImproved/t_delegacion_ocupantes_por_vivienda.csv');
+        console.log(`Archivo 't_delegacion_ocupantes_por_vivienda.csv' creado con exito`);
+
+        const csvCuarto = new ObjectsToCsv(ocupacionCuarto);
+        await csvCuarto.toDisk('./cleanFilesImproved/t_delegacion_ocupantes_por_cuarto.csv');
+        console.log(`Archivo 't_delegacion_ocupantes_por_cuarto.csv' creado con exito`);
+
+    });
+
+
+
+stream.pipe(csvStream);
