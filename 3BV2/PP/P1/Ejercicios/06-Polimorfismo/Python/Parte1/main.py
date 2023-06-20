3f378a71c817c62287e8fd5cc8cfe955683af531
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-20 08:03:58 -0600

    PP: ref: ciclo de repeticion hasta que elija la opcion de salir

--- 3BV2/PP/P1/Ejercicios/06-Polimorfismo/Python/Parte1/main.py
+++ 3BV2/PP/P1/Ejercicios/06-Polimorfismo/Python/Parte1/main.py
@@ -3,26 +3,42 @@ from circulo import Circulo
 from rectangulo import Rectangulo
 from hexagono import Hexagono
 
-print("Seleccione la figura:\n1. Triángulo\n2. Círculo\n3. Rectángulo\n4. Hexágono")
-seleccion = int(input())
+while True:
+    # Mostrar el menú de selección de figuras
+    print("\n\n=====================================================================")
+    print("Seleccione la figura:\n1. Triángulo\n2. Círculo\n3. Rectángulo\n4. Hexágono\n5. Salir")
+    seleccion = int(input())  # Leer la opción seleccionada por el usuario
 
-if seleccion == 1:
-    base = float(input("Ingrese la base del triángulo: "))
-    altura = float(input("Ingrese la altura del triángulo: "))
-    figura = Triangulo(base, altura)
-elif seleccion == 2:
-    radio = float(input("Ingrese el radio del círculo: "))
-    figura = Circulo(radio)
-elif seleccion == 3:
-    largo = float(input("Ingrese el largo del rectángulo: "))
-    ancho = float(input("Ingrese el ancho del rectángulo: "))
-    figura = Rectangulo(largo, ancho)
-elif seleccion == 4:
-    lado = float(input("Ingrese el lado del hexágono: "))
-    figura = Hexagono(lado)
-else:
-    print("Selección inválida")
-    exit()
+    if seleccion == 1:
+        print("\n------------------")
+        base = float(input("Ingrese la base del triángulo: "))  # Leer la base del triángulo
+        print("\n------------------")
+        altura = float(input("Ingrese la altura del triángulo: "))  # Leer la altura del triángulo
+        figura = Triangulo(base, altura)  # Crear una instancia de Triangulo con los valores ingresados
+    elif seleccion == 2:
+        print("\n------------------")
+        radio = float(input("Ingrese el radio del círculo: "))  # Leer el radio del círculo
+        figura = Circulo(radio)  # Crear una instancia de Circulo con el radio ingresado
+    elif seleccion == 3:
+        print("\n------------------")
+        largo = float(input("Ingrese el largo del rectángulo: "))  # Leer el largo del rectángulo
+        print("\n------------------")
+        ancho = float(input("Ingrese el ancho del rectángulo: "))  # Leer el ancho del rectángulo
+        figura = Rectangulo(largo, ancho)  # Crear una instancia de Rectangulo con los valores ingresados
+    elif seleccion == 4:
+        print("\n------------------")
+        lado = float(input("Ingrese el lado del hexágono: "))  # Leer el lado del hexágono
+        figura = Hexagono(lado)  # Crear una instancia de Hexagono con el lado ingresado
+    elif seleccion == 5:
+        print("\n------------------")
+        print("Saliendo del programa...")
+        break  # Salir del ciclo while y terminar el programa
+    else:
+        print("Selección inválida")
+        continue  # Volver al inicio del ciclo para solicitar nuevamente una figura
 
-print("Área: ", figura.calcular_area())
-print("Perímetro: ", figura.calcular_perimetro())
+    # Calcular y mostrar el área y perímetro de la figura seleccionada
+    print("\n------------------")
+    print("Área: ", figura.calcular_area())
+    print("Perímetro: ", figura.calcular_perimetro())
+    print()
