535a4c5712d8e2375c910a777577e4a782035e8a
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-20 05:45:32 -0600

    PP: feat: instancia para probar calculadora

--- 3BV2/PP/P1/Ejercicios/03-POO/Calculadora/Calculadora.py
+++ 3BV2/PP/P1/Ejercicios/03-POO/Calculadora/Calculadora.py
@@ -1,3 +1,4 @@
+# Clase Calculadora que realiza las operaciones básicas de suma, resta, multiplicación, división y módulo
 class Calculadora:
     # Constructor que inicializa los valores de los atributos enteros, flotantes y dobles
     def __init__(self, entero1=10, flotante1=3.14, doble1=5.23456):
@@ -33,3 +34,15 @@ class Calculadora:
     # Método que recibe un entero como argumento y devuelve el módulo del atributo entero1 y el argumento entero2
     def modulo(self, entero2):
         return self.entero1 % entero2
+
+
+# Instancia de la clase Calculadora
+calculadora = Calculadora()
+# definimos una variable apra almacenar el valor 10
+entero2 = 5
+# Pruebas de los métodos de la clase Calculadora
+print(f"Suma de {calculadora.entero1} + {entero2} = {calculadora.sumar(entero2)}")
+print(f"Resta de {calculadora.entero1} - {entero2} = {calculadora.restar(entero2)}")
+print(f"Multiplicación de {calculadora.entero1} * {entero2} = {calculadora.multiplicar(entero2)}")
+print(f"Módulo de {calculadora.entero1} % {entero2} = {calculadora.modulo(entero2)}")
+print(f"División de {calculadora.entero1} / {entero2} = {calculadora.dividir(entero2)}")
