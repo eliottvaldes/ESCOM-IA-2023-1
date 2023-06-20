456d77cfa3bb3d7d602048d19deee086a2351938
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-20 05:07:14 -0600

    PP: feat: nueva funcion lambda explicada

---
+++ 3BV2/PP/P1/Ejercicios/02-calculo_lambda/04-lambda_e.py
@@ -0,0 +1,10 @@
+# Define una función lambda llamada 'starts_with' que verifica si una cadena comienza con 'P'
+# Si la cadena comienza con 'P', la función lambda devuelve True, de lo contrario devuelve False
+starts_with = lambda x: True if x.startswith('P') else False
+print(starts_with('Python'))  # Imprime el resultado de la función 'starts_with' con el argumento 'Python'
+
+# Redefine la función lambda 'starts_with'
+# Si la cadena comienza con 'P', la función lambda devuelve True, de lo contrario devuelve False
+starts_with = lambda x: True if x.startswith('P') else False
+print(starts_with('Java'))  # Imprime el resultado de la función 'starts_with' con el argumento 'Java'
+
