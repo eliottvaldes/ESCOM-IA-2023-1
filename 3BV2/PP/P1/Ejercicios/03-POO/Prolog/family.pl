35a919d38162ec34dc13924947182b08e181130f
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-20 05:39:52 -0600

    PP: feat: ejercicio de prolog de arbol genealógico

---
+++ 3BV2/PP/P1/Ejercicios/03-POO/Prolog/family.pl
@@ -0,0 +1,35 @@
+% Facts: parent(Child, Parent)
+parent(john, mary).
+parent(john, mike).
+parent(mary, alice).
+parent(mary, george).
+parent(mike, lisa).
+parent(mike, james).
+parent(susan, alice).
+parent(susan, george).
+parent(lisa, carol).
+parent(lisa, paul).
+parent(james, karen).
+parent(james, daniel).
+
+% Rule: grandparent(Grandchild, Grandparent)
+grandparent(Grandchild, Grandparent) :-
+    parent(Grandchild, Parent),
+    parent(Parent, Grandparent).
+
+% Rule: sibling(Person1, Person2)
+sibling(Person1, Person2) :-
+    parent(Person1, Parent),
+    parent(Person2, Parent),
+    Person1 \= Person2.
+
+% Rule: cousin(Person1, Person2)
+cousin(Person1, Person2) :-
+    parent(Person1, Parent1),
+    parent(Person2, Parent2),
+    sibling(Parent1, Parent2).
+
+% Rule: greatgrandparent(Greatgrandchild, Greatgrandparent)
+greatgrandparent(Greatgrandchild, Greatgrandparent) :-
+    parent(Greatgrandchild, Parent),
+    grandparent(Parent, Greatgrandparent).
