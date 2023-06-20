4a406ce9b279d9123e8c2575a26953007e07f0ab
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-20 08:07:28 -0600

    PP: ref: repetir proceso ahsta que seleccione opcion salir

--- 3BV2/PP/P1/Ejercicios/06-Polimorfismo/Python/Parte2/main.py
+++ 3BV2/PP/P1/Ejercicios/06-Polimorfismo/Python/Parte2/main.py
@@ -2,30 +2,38 @@ from estudiante import Estudiante
 from docente import Docente
 from paae import PAAE
 
-estudiante = Estudiante('1', 'Juan', 'CURPJUAN1234', 'Calle 123')
-docente = Docente('2', 'Maria', 'CURPMARIA5678', 'Calle 456')
-paae = PAAE('3', 'Carlos', 'CURPCARLOS9012', 'Calle 789')
+# Crear instancias de Estudiante, Docente y PAAE
+estudiante = Estudiante('1', 'Eliot', 'CURPELIOT1234', 'Calle 123')
+docente = Docente('2', 'Jaime', 'CURPJAIME5678', 'Calle 456')
+paae = PAAE('3', 'Itzel', 'CURPITZEL9012', 'Calle 789')
 
-print('Por favor, selecciona una opción:')
-print('1. Estudiante')
-print('2. Docente')
-print('3. PAAE')
+while True:
+    print("\n\n=====================================================================")
+    print('Por favor, selecciona una opción:')
+    print('1. Estudiante')
+    print('2. Docente')
+    print('3. PAAE')
+    print('4. Salir')
 
-opcion = int(input())
+    opcion = int(input())  # Leer la opción seleccionada por el usuario
 
-persona_seleccionada = None
+    if opcion == 1:
+        persona_seleccionada = estudiante
+    elif opcion == 2:
+        persona_seleccionada = docente
+    elif opcion == 3:
+        persona_seleccionada = paae
+    elif opcion == 4:
+        print('Saliendo del programa...')
+        break  # Salir del ciclo while y finalizar el programa
+    else:
+        print('Opción inválida')
+        continue  # Volver al inicio del ciclo para solicitar nuevamente una opción
 
-if opcion == 1:
-    persona_seleccionada = estudiante
-elif opcion == 2:
-    persona_seleccionada = docente
-elif opcion == 3:
-    persona_seleccionada = paae
-else:
-    print('Opción inválida')
-    exit(1)
-
-print('ID:', persona_seleccionada.get_id())
-print('Nombre:', persona_seleccionada.get_nombre())
-print('CURP:', persona_seleccionada.get_curp())
-print('Domicilio:', persona_seleccionada.get_domicilio())
+    # Mostrar la información de la persona seleccionada
+    print("\n------------------")
+    print('ID:', persona_seleccionada.get_id())
+    print('Nombre:', persona_seleccionada.get_nombre())
+    print('CURP:', persona_seleccionada.get_curp())
+    print('Domicilio:', persona_seleccionada.get_domicilio())
+    print()
