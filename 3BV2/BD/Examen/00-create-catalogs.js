35018a8e731842251752ce26625df8b0cca4b297
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-25 02:42:24 -0600

    BD: ref: remplazar archivos csv por sql para importar tabla incidente

--- 3BV2/BD/Examen/00-create-catalogs.js
+++ 3BV2/BD/Examen/00-create-catalogs.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const fastcsv = require('fast-csv');
 const ObjectsToCsv = require('objects-to-csv');
-
 const moment = require('moment');
+
+const { createInsertSqlFileImport } = require('./utils/helper-sql-file-creator');
 
 let delegations = [];
 let classifications = [];
@@ -17,6 +18,9 @@ const stream = fs.createReadStream('./initialFiles/inViales_2022_2023.csv');
 if (!fs.existsSync('./cleanFiles2022')) {
     fs.mkdirSync('./cleanFiles2022');
 }
+if (!fs.existsSync('./cleanFiles2022/sql')) {
+    fs.mkdirSync('./cleanFiles2022/sql');
+}
 
 let csvStream = fastcsv.parse()
     .on('data', (data) => {
@@ -29,13 +33,13 @@ let csvStream = fastcsv.parse()
             4 - fecha_cierre
             5 - hora_cierre
             6 - tipo_incidente_c4
-            7 - incidente_c4
+            7 - id_incidente_c4
             8 - alcaldia_inicio
             9 - latitud
             10 - longitud
-            11 - codigo_cierre
-            12 - clas_con_f_alarma
-            13 - tipo_entrada
+            11 - id_codigo_cierre
+            12 - id_clasificacion
+            13 - id_tipo_entrada
             14 - alcaldia_cierre
             15 - colonia
          */
@@ -65,13 +69,13 @@ let csvStream = fastcsv.parse()
         if (data[8] != 'NA' && data[13] != 'NA') {
             // define the structure of the table 't_incidentes'
             incidentsTable.push({
-                folio: data[0],
-                delegacion_inicio: data[8],
-                delegacion_cierre: data[14],
-                incidente_c4: data[7],
-                tipo_entrada: data[13],
-                clas_con_f_alarma: data[12],
-                codigo_cierre: data[11],
+                incidente_folio: data[0],
+                id_delegacion_inicio: data[8],
+                id_delegacion_cierre: data[14],
+                id_incidente_c4: data[7],
+                id_tipo_entrada: data[13],
+                id_clasificacion: data[12],
+                id_codigo_cierre: data[11],
                 fecha_creacion: data[1],
                 hora_creacion: data[2],
                 fecha_cierre: data[4],
@@ -94,12 +98,12 @@ let csvStream = fastcsv.parse()
 
         // replace the values of the columns with the id of the catalog
         incidentsTable.forEach((incident) => {
-            incident.delegacion_inicio = delegations.indexOf(incident.delegacion_inicio) + 1;
-            incident.delegacion_cierre = delegations.indexOf(incident.delegacion_cierre) + 1;
-            incident.incidente_c4 = incidents.indexOf(incident.incidente_c4) + 1;
-            incident.tipo_entrada = entryTypes.indexOf(incident.tipo_entrada) + 1;
-            incident.clas_con_f_alarma = classifications.indexOf(incident.clas_con_f_alarma) + 1;
-            incident.codigo_cierre = closureCodes.indexOf(incident.codigo_cierre) + 1;
+            incident.id_delegacion_inicio = delegations.indexOf(incident.id_delegacion_inicio) + 1;
+            incident.id_delegacion_cierre = delegations.indexOf(incident.id_delegacion_cierre) + 1;
+            incident.id_incidente_c4 = incidents.indexOf(incident.id_incidente_c4) + 1;
+            incident.id_tipo_entrada = entryTypes.indexOf(incident.id_tipo_entrada) + 1;
+            incident.id_clasificacion = classifications.indexOf(incident.id_clasificacion) + 1;
+            incident.id_codigo_cierre = closureCodes.indexOf(incident.id_codigo_cierre) + 1;
 
             // parse the dates to YYYY-MM-DD
             incident.fecha_creacion = moment(incident.fecha_creacion, 'YYYY-MM-DD').format('YYYY-MM-DD');
@@ -115,6 +119,7 @@ let csvStream = fastcsv.parse()
 
         });
 
+        // create the csv files to import data to the database
         let csv1 = new ObjectsToCsv(delegations.map((del, index) => ({ id_delegacion: index + 1, delegacion: del })));
         await csv1.toDisk('./cleanFiles2022/cat_delegacion.csv');
         console.log(`File cat_delegacion.csv created`);
@@ -135,57 +140,14 @@ let csvStream = fastcsv.parse()
         await csv5.toDisk('./cleanFiles2022/cat_incidente_codigo_cierre.csv');
         console.log(`File cat_incidente_codigo_cierre.csv created`);
 
-
+        // show the total of rows in the table incidents
         console.log(`Total incidents: ${incidentsTable.length} `);
 
-        let csvIncidentesParte1 = new ObjectsToCsv(incidentsTable.slice(0, 30000));
-        await csvIncidentesParte1.toDisk('./cleanFiles2022/t_incidente_parte1.csv');
-        console.log(`File t_incidente_parte1.csv created`);
+        // call the file to import the data to the database
+        await createInsertSqlFileImport('t_incidente', 't_incidentes_p1', incidentsTable.slice(0, 100000));
+        await createInsertSqlFileImport('t_incidente', 't_incidentes_p2', incidentsTable.slice(100000, 200000));
+        await createInsertSqlFileImport('t_incidente', 't_incidentes_p3', incidentsTable.slice(200000, 300000));
 
-        let csvIncidentesParte2 = new ObjectsToCsv(incidentsTable.slice(30000, 60000));
-        await csvIncidentesParte2.toDisk('./cleanFiles2022/t_incidente_parte2.csv');
-        console.log(`File t_incidente_parte2.csv created`);
-
-        let csvIncidentesParte3 = new ObjectsToCsv(incidentsTable.slice(60000, 90000));
-        await csvIncidentesParte3.toDisk('./cleanFiles2022/t_incidente_parte3.csv');
-        console.log(`File t_incidente_parte3.csv created`);
-
-        let csvIncidentesParte4 = new ObjectsToCsv(incidentsTable.slice(90000, 120000));
-        await csvIncidentesParte4.toDisk('./cleanFiles2022/t_incidente_parte4.csv');
-        console.log(`File t_incidente_parte4.csv created`);
-
-        let csvIncidentesParte5 = new ObjectsToCsv(incidentsTable.slice(120000, 150000));
-        await csvIncidentesParte5.toDisk('./cleanFiles2022/t_incidente_parte5.csv');
-        console.log(`File t_incidente_parte5.csv created`);
-
-        let csvIncidentesParte6 = new ObjectsToCsv(incidentsTable.slice(150000, 180000));
-        await csvIncidentesParte6.toDisk('./cleanFiles2022/t_incidente_parte6.csv');
-        console.log(`File t_incidente_parte6.csv created`);
-
-        let csvIncidentesParte7 = new ObjectsToCsv(incidentsTable.slice(180000, 210000));
-        await csvIncidentesParte7.toDisk('./cleanFiles2022/t_incidente_parte7.csv');
-        console.log(`File t_incidente_parte7.csv created`);
-
-        let csvIncidentesParte8 = new ObjectsToCsv(incidentsTable.slice(210000, 240000));
-        await csvIncidentesParte8.toDisk('./cleanFiles2022/t_incidente_parte8.csv');
-        console.log(`File t_incidente_parte8.csv created`);
-
-        let csvIncidentesParte9 = new ObjectsToCsv(incidentsTable.slice(240000, 270000));
-        await csvIncidentesParte9.toDisk('./cleanFiles2022/t_incidente_parte9.csv');
-        console.log(`File t_incidente_parte9.csv created`);
-
-        let csvIncidentesParte10 = new ObjectsToCsv(incidentsTable.slice(270000, 300000));
-        await csvIncidentesParte10.toDisk('./cleanFiles2022/t_incidente_parte10.csv');
-        console.log(`File t_incidente_parte10.csv created`);
-
-        let csvIncidentesParte11 = new ObjectsToCsv(incidentsTable.slice(300000, 330000));
-        await csvIncidentesParte11.toDisk('./cleanFiles2022/t_incidente_parte11.csv');
-        console.log(`File t_incidente_parte11.csv created`);
-
-
-        /*         let csv6 = new ObjectsToCsv(incidentsTable);
-                await csv6.toDisk('./cleanFiles2022/t_incidente.csv');
-                console.log(`File incidentes_viales.csv created`); */
 
     });
 
